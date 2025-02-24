f7e888bb6089a3a01edc32f7d114a0626730ba30
André Augusto <114283648+reshsix@users.noreply.github.com>
2025-02-23 23:40:23 -0300

    Added exchange/secret to command-line tool

--- console.c
+++ console.c
@@ -34,6 +34,7 @@
 #include <maid/hash.h>
 #include <maid/pub.h>
 #include <maid/sign.h>
+#include <maid/kex.h>
 #include <maid/serial.h>
 
 /* Filter functions */
@@ -308,6 +309,16 @@ usage(void)
     fprintf(stderr, "        rsa-pkcs1-sha512     (key: PEM)\n");
     fprintf(stderr, "        rsa-pkcs1-sha512/224 (key: PEM)\n");
     fprintf(stderr, "        rsa-pkcs1-sha512/256 (key: PEM)\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    maid exchange [algorithm] < private\n");
+    fprintf(stderr, "    Generates a public-key for key exchange\n");
+    fprintf(stderr, "    Algorithms:\n");
+    fprintf(stderr, "        dh-group14 (private: 256)\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "    maid secret [algorithm] [private file] < public\n");
+    fprintf(stderr, "    Generates a secret from key exchange\n");
+    fprintf(stderr, "    Algorithms:\n");
+    fprintf(stderr, "        dh-group14 (public: 256, private: 256)\n");
     return false;
 }
 
@@ -588,6 +599,115 @@ sign_verify(int argc, char *argv[], bool verify)
     return ret;
 }
 
+static u8 dh_group14_mod[2048 / 8] =
+    {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xc9, 0x0f, 0xda, 0xa2,
+     0x21, 0x68, 0xc2, 0x34, 0xc4, 0xc6, 0x62, 0x8b, 0x80, 0xdc, 0x1c, 0xd1,
+     0x29, 0x02, 0x4e, 0x08, 0x8a, 0x67, 0xcc, 0x74, 0x02, 0x0b, 0xbe, 0xa6,
+     0x3b, 0x13, 0x9b, 0x22, 0x51, 0x4a, 0x08, 0x79, 0x8e, 0x34, 0x04, 0xdd,
+     0xef, 0x95, 0x19, 0xb3, 0xcd, 0x3a, 0x43, 0x1b, 0x30, 0x2b, 0x0a, 0x6d,
+     0xf2, 0x5f, 0x14, 0x37, 0x4f, 0xe1, 0x35, 0x6d, 0x6d, 0x51, 0xc2, 0x45,
+     0xe4, 0x85, 0xb5, 0x76, 0x62, 0x5e, 0x7e, 0xc6, 0xf4, 0x4c, 0x42, 0xe9,
+     0xa6, 0x37, 0xed, 0x6b, 0x0b, 0xff, 0x5c, 0xb6, 0xf4, 0x06, 0xb7, 0xed,
+     0xee, 0x38, 0x6b, 0xfb, 0x5a, 0x89, 0x9f, 0xa5, 0xae, 0x9f, 0x24, 0x11,
+     0x7c, 0x4b, 0x1f, 0xe6, 0x49, 0x28, 0x66, 0x51, 0xec, 0xe4, 0x5b, 0x3d,
+     0xc2, 0x00, 0x7c, 0xb8, 0xa1, 0x63, 0xbf, 0x05, 0x98, 0xda, 0x48, 0x36,
+     0x1c, 0x55, 0xd3, 0x9a, 0x69, 0x16, 0x3f, 0xa8, 0xfd, 0x24, 0xcf, 0x5f,
+     0x83, 0x65, 0x5d, 0x23, 0xdc, 0xa3, 0xad, 0x96, 0x1c, 0x62, 0xf3, 0x56,
+     0x20, 0x85, 0x52, 0xbb, 0x9e, 0xd5, 0x29, 0x07, 0x70, 0x96, 0x96, 0x6d,
+     0x67, 0x0c, 0x35, 0x4e, 0x4a, 0xbc, 0x98, 0x04, 0xf1, 0x74, 0x6c, 0x08,
+     0xca, 0x18, 0x21, 0x7c, 0x32, 0x90, 0x5e, 0x46, 0x2e, 0x36, 0xce, 0x3b,
+     0xe3, 0x9e, 0x77, 0x2c, 0x18, 0x0e, 0x86, 0x03, 0x9b, 0x27, 0x83, 0xa2,
+     0xec, 0x07, 0xa2, 0x8f, 0xb5, 0xc5, 0x5d, 0xf0, 0x6f, 0x4c, 0x52, 0xc9,
+     0xde, 0x2b, 0xcb, 0xf6, 0x95, 0x58, 0x17, 0x18, 0x39, 0x95, 0x49, 0x7c,
+     0xea, 0x95, 0x6a, 0xe5, 0x15, 0xd2, 0x26, 0x18, 0x98, 0xfa, 0x05, 0x10,
+     0x15, 0x72, 0x8e, 0x5a, 0x8a, 0xac, 0xaa, 0x68, 0xff, 0xff, 0xff, 0xff,
+     0xff, 0xff, 0xff, 0xff};
+
+extern bool
+exchange_secret(int argc, char *argv[], bool secret)
+{
+    bool ret = false;
+
+    if ((!secret && argc == 2) || (secret && argc == 3))
+    {
+        ret = true;
+
+        int in  = STDIN_FILENO;
+        int out = STDOUT_FILENO;
+
+        size_t key_s = 0;
+        maid_kex *ctx = NULL;
+        if (strcmp(argv[1], "dh-group14") == 0)
+        {
+            size_t words = maid_mp_words(2048);
+            key_s = 256;
+
+            maid_mp_word g[words];
+            maid_mp_word p[words];
+
+            maid_mp_mov(words, g, NULL);
+            maid_mp_mov(words, p, NULL);
+
+            struct maid_dh_group group = {.generator = g, .modulo = p};
+
+            g[0] = 2;
+            maid_mp_read(words, p, dh_group14_mod, true);
+            ctx = maid_kex_new(maid_dh, &group, 2048);
+        }
+        else
+            ret = usage();
+
+        if (ret && !ctx)
+        {
+            fprintf(stderr, "Out of memory\n");
+            ret = false;
+        }
+
+
+        if (ret)
+        {
+            ret = false;
+
+            u8 buffer[key_s], buffer2[key_s];
+            if (!secret)
+            {
+                if (read(in, buffer, key_s) == (ssize_t)key_s &&
+                    read(in, buffer, 1) == 0)
+                {
+                    maid_kex_gpub(ctx, buffer, buffer2);
+                    ret = (write(out, buffer2, key_s) == (ssize_t)key_s);
+                }
+                else
+                    fprintf(stderr, "Invalid private key\n");
+            }
+            else
+            {
+                u8 secret[key_s];
+                if (read(in, buffer, key_s) == (ssize_t)key_s &&
+                    read(in, buffer, 1) == 0)
+                {
+                    if (get_data(argv[2], buffer2, key_s, false))
+                    {
+                        maid_kex_gsec(ctx, buffer2, buffer, secret);
+                        ret = (write(out, secret, key_s) == (ssize_t)key_s);
+                    }
+                    else
+                        fprintf(stderr, "Invalid private key\n");
+                }
+                else
+                    fprintf(stderr, "Invalid public key\n");
+                maid_mem_clear(secret,  sizeof(secret));
+            }
+            maid_mem_clear(buffer,  sizeof(buffer));
+            maid_mem_clear(buffer2, sizeof(buffer2));
+        }
+    }
+    else
+        ret = usage();
+
+    return ret;
+}
+
 extern int
 main(int argc, char *argv[])
 {
@@ -608,6 +728,10 @@ main(int argc, char *argv[])
             ret = sign_verify(argc, argv, false);
         else if (strcmp(argv[0], "verify") == 0)
             ret = sign_verify(argc, argv, true);
+        else if (strcmp(argv[0], "exchange") == 0)
+            ret = exchange_secret(argc, argv, false);
+        else if (strcmp(argv[0], "secret") == 0)
+            ret = exchange_secret(argc, argv, true);
         else
             ret = usage();
     }
